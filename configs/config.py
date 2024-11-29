b9d04d9f0accfd421998144ac9cffa69ab63110a
Jithin Krishnan <jithinkrishnan42@gmail.com>
2024-11-29 15:30:01 +0530

    updated config and locator files

--- configs/config.py
+++ configs/config.py
@@ -1,4 +1,20 @@
 # configs/config.py
-class Config:
-    base_url = 'https://example.com'
-    search_text = "Example Search"
+import os
+
+from dotenv import load_dotenv
+
+
+def get_credentials():
+    load_dotenv()  # Load environment variables from .env file
+    credentials = {
+        'email': os.getenv('EMAIL'),
+        'password': os.getenv('PASSWORD'),
+    }
+    return credentials
+
+
+class LoginConfig:
+    credentials = get_credentials()  # Call the function to get credentials
+    base_url = 'https://example.con/'
+    email = credentials['email']
+    password = credentials['password']
--- locators/login_locator.py
+++ locators/login_locator.py
@@ -5,6 +5,3 @@ class LoginLocators:
     email = By.ID, "email"
     password = By.ID, "password"
     sign_in = By.XPATH, "//button[normalize-space()='Sign In']"
-    otp = By.ID, "otp"
-    verify = By.XPATH, "//button[normalize-space()='Verify']"
-    error_message = By.XPATH, "//div[@class='alert alert-danger']"
